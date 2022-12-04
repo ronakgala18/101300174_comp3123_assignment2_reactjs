10eea2f178aa13a9af2dd972a8e6b0024d9c569a
ronakgala18 <ronakgala18@gmail.com>
2022-12-04 16:33:59 -0500

    login page added(css left)

---
+++ src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+    let navigate = useNavigate();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [login, setLogin] = useState("");
+
+    const postData = () => {
+        axios.post("https://101300174-comp-3123-assignment1.vercel.app/api/user/login", {
+            username: username,
+            password: password,
+        },
+        ).then((response) => {
+            navigate("/view")
+        }).catch(error => {
+            console.log(error)
+            if (!error.response.data.status) {
+                setLogin("Invalid username or password!")
+            }
+        });
+    }
+
+    return (
+        <div >
+            <div >
+                <span >Login to your account</span>
+                <div >
+                    <div></div>
+                    <div >
+                        <label > Username </label>
+                        <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md" />
+                        <label > Password</label>
+                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md" />
+                        <p >{login}</p>
+                        <div >
+                            <button onClick={postData} type="submit">Login</button>
+                            <Link to='/register'>
+                                <button  >Register!</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
